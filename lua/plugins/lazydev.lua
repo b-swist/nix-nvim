20038e73a05eaaf55f7692140eeeebda97c97550
b-swist <bswist@protonmail.com>
2025-05-18 20:23:58 +0200

    update lazydev setup

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -3,7 +3,11 @@ return {
     category = "lsp.lua",
     ft = "lua",
     after = function()
+        ---@diagnostic disable-next-line: missing-fields
         require("lazydev").setup({
+            library = {
+                { path = nixCats.nixCatsPath .. "/lua", words = { "nixCats" } },
+            },
             integrations = {
                 cmp = false,
             },
