5a232b2b7d50c3c25203be7444f6e6ed62bd07e7
b-swist <bswist@protonmail.com>
2025-05-25 15:04:21 +0200

    enable formatter only if category is enabled

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -7,8 +7,8 @@ return {
     after = function()
         require("conform").setup({
             formatters_by_ft = {
-                lua = { "stylua" },
-                nix = { "alejandra" },
+                lua = nixCats("format.lua") and { "stylua" } or nil,
+                nix = nixCats("format.nix") and { "alejandra" } or nil,
             },
             format_on_save = {
                 lsp_format = "fallback",
