26f66734798466a760b120780072a118c279c2ce
b-swist <bswist@protonmail.com>
2025-06-08 16:42:09 +0200

    add disabling autoformat

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -9,15 +9,35 @@ return {
     cmd = "ConformInfo",
     after = function()
         local conform = require("conform")
+
+        vim.api.nvim_create_user_command("ConformDisable", function(args)
+            if args.bang then
+                vim.b.disable_autoformat = true
+            else
+                vim.g.disable_autoformat = true
+            end
+        end, { bang = true })
+
+        ---@diagnostic disable: missing-parameter
+        vim.api.nvim_create_user_command("ConformEnable", function()
+            vim.b.disable_autoformat = false
+            vim.g.disable_autoformat = false
+        end)
+
         conform.setup({
+            stop_after_first = true,
             formatters_by_ft = {
                 lua = set_formatter("lua", { "stylua" }),
-                nix = set_formatter("nix", { "alejandra" }),
+                nix = set_formatter("nix", { "alejandra", "nixfmt" }),
                 tex = set_formatter("latex", { "tex-fmt" }),
             },
-            format_on_save = {
-                lsp_format = "fallback",
-            },
+            format_on_save = function(bufnr)
+                local disabled_ft = {}
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                return { lsp_format = disabled_ft[vim.bo[bufnr].filetype] and "never" or "fallback" }
+            end,
         })
 
         vim.keymap.set("n", "grf", conform.format)
