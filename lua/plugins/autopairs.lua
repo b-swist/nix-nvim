17c8c5ba105bc09022d7d87066e88df618900c78
b-swist <bswist@protonmail.com>
2025-06-08 13:48:55 +0200

    add autopairs rules to tex files

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -3,6 +3,23 @@ return {
     category = "general",
     event = "InsertEnter",
     after = function()
-        require("nvim-autopairs").setup()
+        local autopairs, rule = require("nvim-autopairs"), require("nvim-autopairs.rule")
+
+        autopairs.setup({
+            disable_filetype = { "TelescopePrompt", "oil" },
+        })
+
+        table.insert(autopairs.get_rules("'")[1].not_filetypes, "tex")
+
+        autopairs.add_rules({
+            rule("$", "$", "tex"):with_move(function(opts)
+                return opts.next_char == opts.char
+            end),
+            rule("|", "|", "tex"):with_move(function(opts)
+                return opts.next_char == opts.char
+            end):with_pair(function()
+                return vim.fn["vimtex#syntax#in_mathzone"]() == 1
+            end),
+        })
     end,
 }
